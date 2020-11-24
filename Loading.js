60140671ac32c661cf078568a96d0d176ac87aa7
곽경열 <62980973+kwakky1@users.noreply.github.com>
2020-11-24 22:08:22 +0900

    리액트 네이티브로 페이지 만들기, 간단한 로딩페이지 완료

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>오늘의 미친 날씨를 받아보자</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 35,
+        paddingVertical: 350,
+        backgroundColor: '#FDF6AA'
+    },
+    text: {
+        color: '#2c2c2c',
+        fontSize: 30
+    }
+});
